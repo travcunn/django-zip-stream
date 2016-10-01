673a88839ec3842de487af7ab5dbceeab3ffc29e
Travis Cunningham <tcunningham@smartfile.com>
2016-10-01 16:33:49 -0400

    Update docstring.

--- django_zip_stream/__init__.py
+++ django_zip_stream/__init__.py
@@ -6,6 +6,8 @@ except ImportError:
 
 
 class TransferZipResponse(StreamingHttpResponse):
+    """ Streaming zip response. """
+
     def __init__(self, filename, files, content_type="application/zip"):
         """
         Parameters:
@@ -14,7 +16,8 @@ class TransferZipResponse(StreamingHttpResponse):
             be transferred.
         """
 
-        content = "\n".join([self._build_content(x) for x in transfer_files])
+        content = "\n".join(
+            [self._build_content(file_info) for file_info in files])
 
         super(TransferZipResponse, self).__init__(
             content, status=200, content_type=content_type)
