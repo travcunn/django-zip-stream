4ddd5807164561ff89b9518c3976903e91c293bb
Travis Cunningham <tcunningham@smartfile.com>
2016-10-02 10:45:30 -0400

    Remove unused variables.

--- django_zip_stream/settings.py
+++ django_zip_stream/settings.py
@@ -7,6 +7,4 @@ DATABASES = {
 
 SECRET_KEY = '8ij$(7l2!w0f8kggntbv=+$bd=^2xs$cl+3v^_##qjw@2py9_3'
 
-INSTALLED_APPS = (
-    'django_zip_stream',
-)
+INSTALLED_APPS = ('django_zip_stream', )
--- django_zip_stream/tests.py
+++ django_zip_stream/tests.py
@@ -1,5 +1,4 @@
 """ Tests for the TransferZipResponse. """
-from django.http import Http404
 import django.test
 
 from django_zip_stream import TransferZipResponse
@@ -10,7 +9,6 @@ class TransferZipResponseTestCase(django.test.TestCase):
 
     def test_zip_single_file(self):
         """ Test creating a zip for a single file. """
-        request = django.test.RequestFactory().get('/fake-url')
         filename = __file__
         files = [("/chicago.jpg", "/home/django/chicago.jpg", 4096), ]
         response = TransferZipResponse(filename, files)
@@ -23,7 +21,6 @@ class TransferZipResponseTestCase(django.test.TestCase):
 
     def test_zip_multiple_files(self):
         """ Test creating a zip for multiple files. """
-        request = django.test.RequestFactory().get('/fake-url')
         filename = __file__
         files = [("/chicago.jpg", "/home/django/chicago.jpg", 4096),
                  ("/portland.jpg", "/home/django/portland.jpg", 4096), ]
