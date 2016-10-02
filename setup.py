fbd5084ab5e9f5df3abbdae3f779b9ac2e7fbddb
Travis Cunningham <tcunningham@smartfile.com>
2016-10-01 21:53:46 -0400

    Convert markdown README to rst for pypi.

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python
 from setuptools import setup
 
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except (IOError, ImportError):
+    long_description = ''
+
 setup(
     name='django-zip-stream',
     version='0.0.1',
     description='Django extension to assemble ZIP archives dynamically using Nginx with mod_zip.',
+    long_description=long_description,
     author='Travis Cunningham',
     author_email='tcunningham@smartfile.com',
     url='https://github.com/travcunn/django-zip-stream',
